6c5bc560f84046eebea422f22b462809ce9c2841
francescofum <francesco.fum@gmail.com>
2018-05-10 22:12:18 +0100

    ...

--- conductor/setup.py
+++ conductor/setup.py
@@ -1,9 +1,8 @@
 from cx_Freeze import setup,Executable
-
+import dbm
 
 build_exe_options = {'packages': ['dbm']}
 setup(name = "theSoundBytes" ,
       version = "0.1" ,
-      options = {"build_exe": build_exe_options},
       description = "Executable for the soundBytes orchestra" ,
       executables = [Executable("main.py")])
